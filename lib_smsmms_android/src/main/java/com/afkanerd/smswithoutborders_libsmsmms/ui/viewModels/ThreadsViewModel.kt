7f71f7230064323f669946a366b314518be8a266
sherlock <wisdomnji@gmail.com>
2025-10-16 13:09:51 +0100

    update: fixing publishing byte array, need to check modal scopes

--- lib_smsmms_android/src/main/java/com/afkanerd/smswithoutborders_libsmsmms/ui/viewModels/ThreadsViewModel.kt
+++ lib_smsmms_android/src/main/java/com/afkanerd/smswithoutborders_libsmsmms/ui/viewModels/ThreadsViewModel.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.provider.Telephony
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
