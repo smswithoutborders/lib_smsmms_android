93343e6f7f2521bad96b3c33901e77901c9c5ed6
sherlock <wisdomnji@gmail.com>
2025-11-03 13:13:05 +0100

    update: changed settings

--- lib_smsmms_android/src/main/java/com/afkanerd/smswithoutborders_libsmsmms/ui/SettingsMain.kt
+++ lib_smsmms_android/src/main/java/com/afkanerd/smswithoutborders_libsmsmms/ui/SettingsMain.kt
@@ -1,62 +1,44 @@
 package com.afkanerd.smswithoutborders_libsmsmms.ui
 
 import android.content.res.Configuration
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.afkanerd.lib_smsmms_android.R
@@ -284,61 +266,40 @@ fun SettingsItem(
     onClickCallback: (Boolean?) -> Unit,
 ) {
     val inPreviewMode = LocalInspectionMode.current
-    Card(
-        enabled = enabled,
-        onClick = {
-            if(!inPreviewMode) {
-                if(checked != null)
-                    onClickCallback(!checked)
-                else onClickCallback(null)
+    val textColor = if(isWarning) MaterialTheme.colorScheme.onError
+        else MaterialTheme.colorScheme.onSurface
+    HorizontalDivider()
+    ListItem(
+        headlineContent = { Text(itemTitle) },
+        supportingContent = { itemDescription?.let {Text(itemDescription)} },
+        trailingContent = {
+            checked?.let {
+                Switch(
+                    checked = checked,
+                    onCheckedChange = {
+                        onClickCallback(it)
+                    }
+                )
             }
         },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        colors = CardDefaults.cardColors(
-            if(isWarning) MaterialTheme.colorScheme.error
-            else MaterialTheme.colorScheme.surface
+        modifier = if(inPreviewMode) Modifier else Modifier.clickable(onClick = {
+            if(checked != null)
+                onClickCallback(!checked)
+            else onClickCallback(null)
+        }, enabled = enabled),
+        colors = ListItemColors(
+            containerColor = if(isWarning) MaterialTheme.colorScheme.error
+            else MaterialTheme.colorScheme.surface,
+            headlineColor = textColor,
+            leadingIconColor = textColor,
+            overlineColor = textColor,
+            supportingTextColor = textColor,
+            trailingIconColor = textColor,
+            disabledHeadlineColor = textColor,
+            disabledLeadingIconColor = textColor,
+            disabledTrailingIconColor = textColor
         )
-    ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    itemTitle,
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(bottom=8.dp),
-                    style = MaterialTheme.typography.titleSmall,
-                    color = if(isWarning) MaterialTheme.colorScheme.onError else
-                        MaterialTheme.colorScheme.onSurface
-                )
-                itemDescription?.let {
-                    Text(
-                        it,
-                        fontSize = 14.sp,
-                        style = MaterialTheme.typography.bodySmall,
-                        color = if(isWarning) MaterialTheme.colorScheme.surfaceVariant
-                        else MaterialTheme.colorScheme.onSurface
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-
-            if(checked != null) {
-                Column {
-                    Switch(
-                        checked = checked,
-                        onCheckedChange = {
-//                        checked = it
-                            onClickCallback(it)
-                        }
-                    )
-                }
-            }
-        }
-    }
+    )
 }
 
 @Preview
